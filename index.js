b481c23426007e825fd0966be6bb16c441e7c2db
alefibertellobos-code <alefibertellobos@gmail.com>
2026-06-28 21:14:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const fetch = require('node-fetch');
-const nodemailer = require('nodemailer/lib/nodemailer');
+const nodemailer = require('nodemailer');
 
 const app = express();
 app.use(express.json());
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') return res.status(200).end();
-  next();
-});
 
 const SUPABASE_URL = 'https://rgmbmoasuqdiwavcaskl.supabase.co';
 const SUPABASE_KEY = process.env.SUPABASE_KEY;
